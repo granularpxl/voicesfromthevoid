c100b03c3538ccd08f33405870f74272b7b99450
granularpxl <53169513+granularpxl@users.noreply.github.com>
2022-08-25 18:31:26 +1000

    Add files via upload

---
+++ VL53L5CXv4.py
@@ -0,0 +1,40 @@
+import time
+import numpy
+import vl53l5cx_ctypes as vl53l5cx
+from vl53l5cx_ctypes import STATUS_RANGE_VALID, STATUS_RANGE_VALID_LARGE_PULSE
+from pythonosc.udp_client import SimpleUDPClient
+
+ip = "127.0.0.1"
+udpport = 1337
+client = SimpleUDPClient(ip, udpport)
+
+print("Uploading firmware, please wait...")
+vl53 = vl53l5cx.VL53L5CX()
+print("Done!")
+vl53.set_resolution(4*4)
+vl53.set_ranging_frequency_hz(15)
+vl53.set_ranging_mode(1)
+vl53.set_integration_time_ms(5)
+
+
+vl53.start_ranging()
+
+while True:
+    if vl53.data_ready():
+        data = vl53.get_data()
+        distanceraw = numpy.array(data.distance_mm, dtype=numpy.float64)[0, 4:12].reshape((2, 4))
+
+        # 2d array of good ranging data
+        status = numpy.isin(numpy.array(data.target_status)[0, 4:12].reshape((2, 4)), (STATUS_RANGE_VALID))
+        distancevalid = numpy.multiply(distanceraw, status)
+        
+        # substitute zeros for 4000)
+        distancevalid[distancevalid == 0] = 4000
+        distancemin = distancevalid.min()
+        
+        #print(distancevalid)
+        #print(distanceraw)
+        #print(distancemin)
+        client.send_message("/distance", distancemin)
+        
+    time.sleep(0.1)
---
+++ me007.py
@@ -0,0 +1,42 @@
+import time
+import serial
+from pythonosc.udp_client import SimpleUDPClient
+
+SERIAL_PORT = "/dev/ttyS0"   # or "COM4" or whatever
+
+serialport = serial.Serial(SERIAL_PORT, 9600)
+
+ip = "127.0.0.1"
+udpport = 1337
+
+client = SimpleUDPClient(ip, udpport)  # Create client
+
+def read_me007ys(ser, timeout = 1.0):
+    ts = time.monotonic()
+    buf = bytearray(3)
+    idx = 0
+
+    while True:
+        # Option 1, we time out while waiting to get valid data
+        if time.monotonic() - ts > timeout:
+            raise RuntimeError("Timed out waiting for data")
+
+        c = ser.read(1)[0]
+        #print(c)
+        if idx == 0 and c == 0xFF:
+            buf[0] = c
+            idx = idx + 1
+        elif 0 < idx < 3:
+            buf[idx] = c
+            idx = idx + 1
+        else:
+            chksum = sum(buf) & 0xFF
+            if chksum == c:
+                return (buf[1] << 8) + buf[2]
+            idx = 0
+    return None
+
+while True:
+    dist = read_me007ys(serialport)
+    # print("Distance = %d mm" % dist)
+    client.send_message("/distance", dist)
